614b5d45314835eff6f68847e000b01a06692c0f
sivasakthi_a8 <sivasakthi@autonom8.com>
2019-10-02 08:28:52 +0530

    arun project completely done

--- src/App.js
+++ src/App.js
@@ -4,50 +4,53 @@ import "./App.css";
 
 class Multiplevalue extends Component {
   state = {
-    stateofB: this.props.data,
+    stateofText: "",
     multipleaccess: false
   };
+  componentDidMount() {
+    this.setState({ stateofText: this.props.data });
+  }
   render() {
     return (
       <ul>
         <li
           onClick={() =>
-            this.setState({ stateofB: this.props.data, multipleaccess: true })
+            this.setState({ stateofText: this.props.data, multipleaccess: true })
           }
         >
-          {this.state.stateofB + "1"}
+          {this.state.stateofText}
           {this.state.multipleaccess && (
-            <Multiplevalue data={this.state.stateofB + 1} />
+            <Multiplevalue data={this.state.stateofText + 1} />
           )}
         </li>
       </ul>
     );
   }
 }
-
 class App extends Component {
   state = {
     stateofB: "B",
-    multipleaccess: false
+    multiplePlusValue: [],
+    countB: 1
+  };
+
+  increment = () => {
+    this.setState({ countB: this.state.countB + 1 });
+    this.state.multiplePlusValue.push(this.state.stateofB + this.state.countB);
   };
   render() {
     return (
       <div>
         <ul>
-          <li
-            onClick={() =>
-              this.setState({ stateofB: "B", multipleaccess: true })
-            }
-          >
-            B
-            {this.state.multipleaccess && (
-              <Multiplevalue data={this.state.stateofB} />
-            )}
+          <li>
+            <label onClick={this.increment}>B</label>
+            {this.state.multiplePlusValue.map((data, index) => {
+              return <Multiplevalue data={data} key={index} />;
+            })}
           </li>
         </ul>
       </div>
     );
   }
 }
-
 export default App;
