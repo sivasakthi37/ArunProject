473c8609853a431bd308150de91ac4cfad6eeab2
sivasakthi_a8 <sivasakthi@autonom8.com>
2019-10-04 04:28:36 +0530

    project minimized

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import "./App.css";
 class Multiplevalue extends Component {
   state = {
     stateofText: "",
-    array: [],
+    Textarray: [],
     count: 1
   };
   componentDidMount() {
@@ -13,7 +13,7 @@ class Multiplevalue extends Component {
   }
   increment = () => {
     this.setState({ count: this.state.count + 1 });
-    this.state.array.push(this.state.stateofText + this.state.count);
+    this.state.Textarray.push(this.state.stateofText + this.state.count);
   };
   render() {
     return (
@@ -21,75 +21,26 @@ class Multiplevalue extends Component {
         <li
           onClick={() =>
             this.setState({
-              stateofText: this.props.data,
+              stateofText: this.props.data
             })
           }
         >
           <label onClick={this.increment}>{this.state.stateofText}</label>
         </li>
-        {this.state.array.map((data, index) => {
+        {this.state.Textarray.map((data, index) => {
           return <Multiplevalue data={data} key={index} />;
         })}
       </ul>
     );
   }
 }
-class App extends Component {
-  state = {
-    stateofA: "A",
-    stateofB: "B",
-    stateofC: "C",
-    multiplePlusValueB: [],
-    multiplePlusValueA: [],
-    multiplePlusValueC: [],
-    countA: 1,
-    countB: 1,
-    countC: 1
-  };
-
-  increment = Text => {
-    if (Text === "B") {
-      this.setState({ countB: this.state.countB + 1 });
-      this.state.multiplePlusValueB.push(
-        this.state.stateofB + this.state.countB
-      );
-    } else if (Text === "A") {
-      this.setState({ countA: this.state.countA + 1 });
-      this.state.multiplePlusValueA.push(
-        this.state.stateofA + this.state.countA
-      );
-    } else if (Text === "C") {
-      this.setState({ countC: this.state.countC + 1 });
-      this.state.multiplePlusValueC.push(
-        this.state.stateofC + this.state.countC
-      );
-    }
-  };
-  render() {
-    return (
-      <div>
-        <ul>
-          <li>
-            <label onClick={() => this.increment("A")}>A</label>
-            {this.state.multiplePlusValueA.map((data, index) => {
-              return <Multiplevalue data={data} key={index} />;
-            })}
-          </li>
-          <li>
-            <label onClick={() => this.increment("B")}>B</label>
-            {this.state.multiplePlusValueB.map((data, index) => {
-              return <Multiplevalue data={data} key={index} />;
-            })}
-          </li>
-          <li>
-            <label onClick={() => this.increment("C")}>C</label>
-            {this.state.multiplePlusValueC.map((data, index) => {
-              return <Multiplevalue data={data} key={index} />;
-            })}
-          </li>
-        </ul>
-      </div>
-    );
-  }
+function App() {
+  return (
+    <div>
+      <Multiplevalue data={"A"} />
+      <Multiplevalue data={"B"} />
+      <Multiplevalue data={"C"} />
+    </div>
+  );
 }
 export default App;
